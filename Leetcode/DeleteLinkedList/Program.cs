5658fb7655f879956b21b77132b8fe631feb35b2
Sairam Madem (Aricent Technologies Mauritius) <v-samade@microsoft.com>
2018-12-21 19:49:24 -0800

    Delete a list - Iterative and Recursive methods.

---
+++ Leetcode/DeleteLinkedList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteLinkedList
+{
+    class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void DeleteList_Recursion(ListNode head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            DeleteList_Recursion(head.next);
+
+            head.next = null;
+        }
+
+        public static void DeleteList_Iterative(ListNode head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            ListNode temp = head;
+
+            while(head != null)
+            {
+                head.next = null;
+                temp = temp.next;
+                head = temp;
+            }
+        }
+    }
+}
